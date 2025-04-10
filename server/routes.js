1c2c263f93b28136003e5ba12cb08cba59873e29
abhishek bohra <41362461-abhishekbohra20@users.noreply.replit.com>
2025-04-10 15:24:10 +0000

    Restored to 'f682880cd28e93d04431ff20a47e38c245e53d01'
    Replit-Restored-To: f682880cd28e93d04431ff20a47e38c245e53d01

--- server/routes.js
+++
@@ -1,8 +0,0 @@
-
-import { createServer } from "http";
-import { storage } from "./storage.js";
-
-export async function registerRoutes(app) {
-  const httpServer = createServer(app);
-  return httpServer;
-}
